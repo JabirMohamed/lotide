22b7ed5740aa9ef1a02b2722465858fdec786fa7
Jabir Mohamed <jabir.mohamed12@gmail.com>
2023-06-03 11:09:09 -0600

    Creating a function to find the middle element of an array

---
+++ middle.js
@@ -0,0 +1,48 @@
+function eqArrays(actual, expected) {
+  if(actual.length !== expected.length) {
+    return false;
+  }
+
+  //Iterate over eatch item in both arrays and compare them
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+
+  //Array is the same length and all elements are equal
+  return true;
+}
+
+function assertArraysEqual(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log("The assertion passed")
+  } if (eqArrays(array1, array2) !== true) {
+    console.log("The assertion failed")
+  }
+}
+
+
+let middle = function(array){
+  let final = []
+  if (array.length === 1 || array.length === 2){
+    return final
+  }
+  if (array.length % 2 !== 0){
+    let middleIndex = Math.floor(array.length / 2)
+    let middleElement = array[middleIndex]
+    return middleElement
+  }
+  if (array.length % 2 === 0){
+    let middleIndex1 = (array.length / 2) - 1;
+    let middleIndex2 = (array.length / 2);
+    let middleElement1 = array[middleIndex1]
+    let middleElement2 = array[middleIndex2]
+    final.push(middleElement1)
+    final.push(middleElement2)
+  } return final
+}
+
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+console.log(middle(list))
