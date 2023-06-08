1f6b46271d087881353d7191665f09ac5cbebc36
Jabir Mohamed <jabir.mohamed12@gmail.com>
2023-06-08 12:41:22 -0600

    imported dependencies and validated the test file and original file communicate properly.

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require(`../assertArraysEqual`)
+
+const middle = require(`../middle`)
+
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+console.log(middle(list))
