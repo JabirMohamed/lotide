ce849152baa89539565f026a5421c303b5fc70ff
Jabir Mohamed <jabir.mohamed12@gmail.com>
2023-06-08 12:41:56 -0600

    refactored for effeciency

--- middle.js
+++ middle.js
@@ -1,26 +1,28 @@
-function eqArrays(actual, expected) {
-  if(actual.length !== expected.length) {
-    return false;
-  }
+// function eqArrays(actual, expected) {
+//   if(actual.length !== expected.length) {
+//     return false;
+//   }
 
-  //Iterate over eatch item in both arrays and compare them
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
+//   //Iterate over eatch item in both arrays and compare them
+//   for (let i = 0; i < actual.length; i++) {
+//     if (actual[i] !== expected[i]) {
+//       return false;
+//     }
+//   }
 
-  //Array is the same length and all elements are equal
-  return true;
-}
+//   //Array is the same length and all elements are equal
+//   return true;
+// }
 
-function assertArraysEqual(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log("The assertion passed")
-  } if (eqArrays(array1, array2) !== true) {
-    console.log("The assertion failed")
-  }
-}
+const assertArraysEqual = require(`./assertArraysEqual`)
+
+// function assertArraysEqual(array1, array2) {
+//   if (eqArrays(array1, array2) === true) {
+//     console.log("The assertion passed")
+//   } if (eqArrays(array1, array2) !== true) {
+//     console.log("The assertion failed")
+//   }
+// }
 
 
 let middle = function(array){
@@ -43,6 +45,4 @@ let middle = function(array){
   } return final
 }
 
-let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-console.log(middle(list))
+module.exports = middle
