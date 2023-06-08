515e3088f69724e864c0b58052bf9338ee6abc0c
Jabir Mohamed <jabir.mohamed12@gmail.com>
2023-06-08 10:24:20 -0600

    refactoring code so it can be exported and used else where. Also removed test code

--- head.js
+++ head.js
@@ -1,17 +1,8 @@
+const assertEqual = require(`./assertEqual`)
+
+
 function head(array){
   return array[0]
 };
 
-
-
-const assertEqual = function(actual, expected){
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-assertEqual(head([5,6,7]), 7);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+module.exports = head;
